82d014c5ec04dfff4f2e88f8760b6b4d5e0a0d51
GabrielScarpelin <gabriel.scaarpelin319@outlook.com>
2023-03-16 23:41:20 -0300

    Finalizado a página de step 3 e quase finalizando a step 4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 let paginaAtual = 1
+let monthlyPlan = true
+let addons = [false, false, false] 
+let opSelected = 0
+const addonsTeste = {
+    0: {
+        name : 'Online service',
+        active: false
+    },
+    1: {
+        name : 'Larger storage',
+        active: false
+    },
+    2: {
+        name: 'customizableProfile',
+        active: false
+    }
+
+}
 HTMLCollection.prototype.forEachCollection = function (callback){
     const tamanho = this.length
     for (let i = 0; i < tamanho; i++){
-        callback(this.item(i))
+        callback(this.item(i), i)
     }
 }
 
@@ -14,12 +32,59 @@ function proximaPagina(){
             document.getElementById(`step${paginaAtual + 1}`).style.display = 'block'
             document.getElementsByClassName('stepNumber').item(paginaAtual - 1).classList.remove('active')
             document.getElementsByClassName('stepNumber').item(paginaAtual).classList.add('active')
+            document.getElementById('goBack').style.visibility = 'visible'
             paginaAtual++
+        }   
+    }
+    else if (paginaAtual === 2){
+        const addonsPrice = [1, 2, 2]
+        const inputs = document.getElementsByName("planSelecet")
+        if (!inputs[0].checked && !inputs[1].checked && !inputs[2].checked){
+            return
         }
-        
+        inputs.forEach((element, index)=>{
+            if (element.checked){
+                opSelected = index
+            }
+        })
+        currentPage.style.display = 'none';
+        if (monthlyPlan){
+            document.getElementsByClassName('priceAddons').forEachCollection((element, index)=>{
+                element.innerText = `$${addonsPrice[index]}/mo`
+            })
+        }
+        else {
+            document.getElementsByClassName('priceAddons').forEachCollection((element, index)=>{
+                element.innerText = `$${addonsPrice[index] * 10}/yr`
+            })
+        }
+        document.getElementById(`step${paginaAtual + 1}`).style.display = 'block';
+        document.getElementsByClassName('stepNumber').item(paginaAtual - 1).classList.remove('active')
+        document.getElementsByClassName('stepNumber').item(paginaAtual).classList.add('active')
+        paginaAtual++
+    }
+    else if (paginaAtual === 3){
+        const checkboxs = document.querySelectorAll('input[type="checkbox"]').forEach((element, index)=>{
+            if (element.checked) addonsTeste[index].active = true
+        })
+        currentPage.style.display = 'none'
+        document.getElementById(`step${paginaAtual + 1}`).style.display = 'block'
+        document.getElementsByClassName('stepNumber').item(paginaAtual - 1).classList.remove('active')
+        document.getElementsByClassName('stepNumber').item(paginaAtual).classList.add('active')
+        paginaAtual++
     }
 }
-
+function voltarPage(){
+    const currentPage = document.getElementById(`step${paginaAtual}`)
+    if (paginaAtual > 1){
+        currentPage.style.display = 'none'
+        document.getElementById(`step${paginaAtual - 1}`).style.display = 'block'
+        paginaAtual--
+    }
+    if (paginaAtual === 1){
+        document.getElementById('goBack').style.visibility = 'hidden'
+    }
+}
 function validarCampo(){
     const inputs = document.getElementsByClassName("inputTypeText")
     let validate = true
@@ -51,15 +116,22 @@ function removerErroColor(index){
 }
 
 function changePlan(){
+    const price = [9, 12, 15]
     const yearly = document.getElementById('yearly')
     const monthly = document.getElementById('monthly')
     if (monthly.checked){
         yearly.click()
-        console.log("yearly selected")
+        document.getElementsByClassName('price').forEachCollection((element, index)=>{
+            element.innerText = `$${price[index] * 10}/yr`
+        })
+        monthlyPlan = false
     }
     else{
         monthly.click()
-        console.log("monthly selected")
-
+        document.getElementsByClassName('price').forEachCollection((element, index)=>{
+            element.innerText = `$${price[index]}/mo`
+        })
+        monthlyPlan = true
     }
+    
 }
